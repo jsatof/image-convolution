35cf2e6c6991a653de6b31d55a2087ef4122909f
jsferrarelli <ferrarellij@wit.edu>
2020-08-12 07:03:07 -0400

    Refactor convolution calls. Simplified the parameters

--- sobel/serial.cpp
+++ sobel/serial.cpp
@@ -3,10 +3,45 @@
 #include <opencv2/opencv.hpp>
 
 std::string get_image_name(std::string arg);
-void convolve(int *matrix, int height, int width, int *kernel, int k);
-void reduce_matrix(int *matrix_a, int *matrix_b, int *result_matrix, int height, int width);
 void image_to_matrix(cv::Mat image, int *matrix);
 void matrix_to_image(cv::Mat image, int *matrix);
+
+void convolve(int *input, int *output, int height, int width, double *kernel, int kernel_length) {
+    int k = kernel_length / 2;
+
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+
+            double sum = 0;
+
+            for(int u = -k; u <= k; u++) {
+                for(int v = 0; v < kernel_length; v++) {
+                    if(i + u >= 0 && i + u < height) {
+                        if(j + v >= 0 && j + v < width) {
+                            sum += input[(i + u) * width + (j + v)] * kernel[(u + k) * kernel_length + (v + k)];
+                        }
+                    }
+                }
+            }
+
+            output[i * width + j] =  (int) sum;
+
+        }
+    }
+}
+
+// assumes all matrices are equal size
+// uses pythagorean theorem to get magnitudes of the 2 input matrices
+void calc_magnitude(int *matrix_a, int *matrix_b, int *result_matrix, int height, int width) {
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+            int a = matrix_a[i * width + j];
+            int b = matrix_b[i * width + j];
+
+            result_matrix[i * width + j] = (int) sqrtf(a * a + b * b);
+        }
+    }
+}
 
 int main(int argc, char **argv) {
     if(argc != 2) {
@@ -34,37 +69,47 @@ int main(int argc, char **argv) {
     image_to_matrix(grey_image, matrix_x);
     image_to_matrix(grey_image, matrix_y);
 
-    int kernel_length = 3;
+    int kernel_length = 5;
 
-    int sobel_x[9] = {
-        1, 0, -1,
-        2, 0, -2,
-        1, 0, -1
+    // these kernels are not my discovery. 
+    // courtesy of https://stackoverflow.com/questions/9567882/sobel-filter-kernel-of-large-size/41065243#41065243
+    double sobel_x[25] = {
+        -2/8, -1/5,  0,  1/5,  2/8,
+        -2/5, -1/2,  0,  1/2,  2/5,
+        -2/4, -1/1,  0,  1/1,  2/4,
+        -2/5, -1/2,  0,  1/2,  2/5,
+        -2/8, -1/5,  0,  1/5,  2/8
     };
 
-    int sobel_y[9] = {
-        1, 2, 1,
-        0, 0, 0,
-        -1, -2, -1
+    double sobel_y[25] = {
+        -2/8, -2/5, -2/4, -2/5, -2/8,
+        -1/5, -1/2, -1/1, -1/2, -1/5,
+        0,    0,    0,    0,    0,
+        1/5, 1/2, 1/1, 1/2, 1/5,
+        -2/8, -2/5, -2/4, -2/5, -2/8,
     };
+
+    int *result_x = (int*) malloc(height * width * sizeof(int));
+    int *result_y = (int*) malloc(height * width * sizeof(int));
+    int *combined_result = (int*) malloc(height * width * sizeof(int));
 
     long start_time = clock();
-    convolve(matrix_x, height, width, sobel_x, kernel_length);
-    convolve(matrix_y, height, width, sobel_y, kernel_length);
+    convolve(matrix_x, result_x, height, width, sobel_x, kernel_length);
+    convolve(matrix_y, result_y, height, width, sobel_y, kernel_length);
+    calc_magnitude(result_x, result_y, combined_result, height, width);
     long end_time = clock();
 
     double total_time = (double) (end_time - start_time) / CLOCKS_PER_SEC;
-    std::cout << "Convolution Time: " << total_time << "s" << std::endl;
+    printf("Convolution Time: %fs\n", total_time);
 
-    int *result_matrix = (int*) malloc(height * width * sizeof(int));
-    reduce_matrix(matrix_x, matrix_y, result_matrix, height, width);
-
-    matrix_to_image(grey_image, result_matrix);
+    matrix_to_image(grey_image, combined_result);
     cv::imwrite("output_serial.jpg", grey_image);
 
     free(matrix_x);
     free(matrix_y);
-    free(result_matrix);
+    free(result_x);
+    free(result_y);
+    free(combined_result);
     return 0;
 }
 
@@ -82,51 +127,6 @@ std::string get_image_name(std::string arg) {
         return "../images/nier.jpg";
     }
     return "invalid";
-}
-
-void convolve(int *matrix, int height, int width, int *kernel, int kernel_length) {
-    int k = kernel_length / 2;
-
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-
-            double sum = 0;
-
-            for(int u = -k; u <= k; u++) {
-                for(int v = -k; v <= k; v++) {
-
-                    if(i + u >= 0 && i + u < height &&
-                       j + v >= 0 && j + v < width) {
-
-                           sum += kernel[(u + k) * kernel_length + (v + k)] * matrix[(i + u) * width + (j + v)];
-                    }
-
-                }
-            }
-
-            double pixel_value = sum / pow(kernel_length, 2);
-            if(pixel_value < 0)
-                pixel_value = 0;
-            if(pixel_value > 255)
-                pixel_value = 255;
-
-            matrix[i * width + j] =  (int) pixel_value;
-
-        }
-    }
-}
-
-// assumes all matrices are equal size
-// uses pythagorean theorem to get magnitudes of the 2 input matrices
-void reduce_matrix(int *matrix_a, int *matrix_b, int *result_matrix, int height, int width) {
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-            int a = matrix_a[i * width + j];
-            int b = matrix_b[i * width + j];
-
-            result_matrix[i * width + j] = (int) sqrtf(a * a + b * b);
-        }
-    }
 }
 
 void image_to_matrix(cv::Mat image, int *matrix) {
